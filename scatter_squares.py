aa5dc2df8df1a9333fb734e1fa2844cd121ef1c0
Barry Connolly <barry.connolly84@gmail.com>
2022-06-12 19:46:45 +0100

    colours

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [i**2 for i in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.cool, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -14,6 +14,9 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axix.
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
 
