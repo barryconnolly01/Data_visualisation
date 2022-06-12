665d5741c484514b51ba6f7b62d0c5f6c3793871
Barry Connolly <barry.connolly84@gmail.com>
2022-06-12 19:01:49 +0100

    scatter list

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=200)
 
 # Set chart title and label axes.
-ax.set_title("Sqaure Numbers", fontsize=20)
+ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
-ax.tick_params(axis='both', labelsize=10)
+ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
 
-#python mpl_squares.py
+# python scatter_squares.py
